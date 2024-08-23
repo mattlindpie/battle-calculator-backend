e02120e2cd69d70e31f3ed075299b52f9dc1756c
Mattison Pierce <129309089+mattlindpie@users.noreply.github.com>
2024-08-23 13:04:44 -0500

    Add files via upload
    initial commit

---
+++ battlecalculatorbackend/battlecalculatorbackend/src/main/java/com/bcbackend/battlecalculatorbackend/BattlecalculatorbackendApplication.java
@@ -0,0 +1,13 @@
+package com.bcbackend.battlecalculatorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattlecalculatorbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattlecalculatorbackendApplication.class, args);
+	}
+
+}
---
+++ battlecalculatorbackend/battlecalculatorbackend/src/test/java/com/bcbackend/battlecalculatorbackend/BattlecalculatorbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcbackend.battlecalculatorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattlecalculatorbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
